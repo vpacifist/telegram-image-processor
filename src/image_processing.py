e9c60fbe063799707124ac08ade5c6723829960a
vpacifist <v@vovagusev.ru>
2024-11-17 21:34:24 +0400

    Add function to blur user avatar using GaussianBlur

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,5 +1,5 @@
 from rembg import remove
-from PIL import Image
+from PIL import Image, ImageFilter
 
 def crop_image(input_image_path, output_image_path, crop_box):
     """
@@ -26,3 +26,18 @@ def remove_background(input_image_path, output_image_path):
         output = remove(img)
         # Сохраняем изображение с удалённым фоном
         output.save(output_image_path)
+
+def blur_avatar(input_image_path, output_image_path, blur_radius=10):
+    """
+    Применение размытия к аватарке.
+
+    :param input_image_path: Путь к исходной аватарке.
+    :param output_image_path: Путь для сохранения размытой аватарки.
+    :param blur_radius: Радиус размытия (по умолчанию 10).
+    """
+    # Открываем изображение с помощью PIL
+    with Image.open(input_image_path) as img:
+        # Применяем фильтр GaussianBlur
+        blurred_image = img.filter(ImageFilter.GaussianBlur(blur_radius))
+        # Сохраняем изображение с эффектом размытия
+        blurred_image.save(output_image_path)
