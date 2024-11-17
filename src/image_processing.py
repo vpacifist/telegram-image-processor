ca7e95861348f098816182411adceacec3f73e00
vpacifist <v@vovagusev.ru>
2024-11-17 21:29:20 +0400

    Add function to remove background using rembg

--- src/image_processing.py
+++ src/image_processing.py
@@ -1,3 +1,4 @@
+from rembg import remove
 from PIL import Image
 
 def crop_image(input_image_path, output_image_path, crop_box):
@@ -11,3 +12,17 @@ def crop_image(input_image_path, output_image_path, crop_box):
     with Image.open(input_image_path) as img:
         cropped_image = img.crop(crop_box)
         cropped_image.save(output_image_path)
+
+def remove_background(input_image_path, output_image_path):
+    """
+    Удаление фона с изображения.
+
+    :param input_image_path: Путь к исходному изображению.
+    :param output_image_path: Путь для сохранения изображения с удалённым фоном.
+    """
+    # Открываем изображение с помощью PIL
+    with Image.open(input_image_path) as img:
+        # Применяем функцию удаления фона
+        output = remove(img)
+        # Сохраняем изображение с удалённым фоном
+        output.save(output_image_path)
